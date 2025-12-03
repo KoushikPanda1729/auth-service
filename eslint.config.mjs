ce637d5c0a215fb5bcd452371089b738e5b7324e
KoushikPanda1729 <programmer747767@gmail.com>
2025-12-03 14:13:22 +0530

    Refactor configuration files for consistency and clarity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,24 @@
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    ignores: ['dist/**', 'node_modules/**', '**/*.config.mjs'],
-
-  },
-  
-  {
-
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: new URL('.', import.meta.url).pathname,
-      },
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        ignores: ["dist/**", "node_modules/**", "**/*.config.mjs"],
     },
-    rules : {
-        "no-console":"off",
-        "dot-notation":"error"
+
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: new URL(".", import.meta.url).pathname,
+            },
+        },
+        rules: {
+            "no-console": "off",
+            "dot-notation": "error",
+        },
     }
-  },
-  
 );
