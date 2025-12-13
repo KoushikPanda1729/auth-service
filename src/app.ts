1ee39b35088ba9080be171e5ebaced74cde6fbb9
KoushikPanda1729 <programmer747767@gmail.com>
2025-12-13 16:06:42 +0530

    Add status code and remove unused code

--- src/app.ts
+++ src/app.ts
@@ -76,7 +76,7 @@ app.use("/auth", authRouter);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
     logger.error(err.message);
-    const statusCode = err.status || 500;
+    const statusCode = err.status || err.statusCode || 500;
     res.status(statusCode).json({
         errors: [
             {
